42ffa5c43fff05a24342ec443ee6f4480f7b7f18
Daniel Moura <daniel-moura@outlook.com>
2022-12-31 16:16:42 -0300

    feat: service create for brand

---
+++ src/services/CreateBrandService.ts
@@ -0,0 +1,22 @@
+import { IBrandsRepository } from '../repositories/IBrandsRepository';
+
+interface IRequest {
+  name: string;
+  history: string;
+}
+
+class CreateBrandService {
+  constructor(private brandsRepository: IBrandsRepository) {}
+
+  execute({ name, history }: IRequest): void {
+    const brandAlreadyExists = this.brandsRepository.findByName(name);
+
+    if (brandAlreadyExists) {
+      throw new Error('Brand Already Exists!');
+    }
+
+    this.brandsRepository.create({ name, history });
+  }
+}
+
+export { CreateBrandService };
