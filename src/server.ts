dcaee13809312672c0c8d1ee892e6245d39d3472
Daniel Moura <daniel-moura@outlook.com>
2022-12-31 15:17:13 -0300

    feat: add express json and brands routes on app

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
 import express from 'express';
 
+import { brandsRoutes } from './routes/brands.routes';
+
 const app = express();
+
+app.use(express.json());
+
+app.use('/brands', brandsRoutes);
 
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to Shop Car!' });
