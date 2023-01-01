ea12dc5e5ced1576e57b2744d3c8befc3b5b02f3
Daniel Moura <daniel-moura@outlook.com>
2023-01-01 16:08:07 -0300

    feat: read data file

--- src/modules/cars/useCases/importBrand/ImportBrandUseCase.ts
+++ src/modules/cars/useCases/importBrand/ImportBrandUseCase.ts
@@ -1,6 +1,46 @@
+import { parse } from 'csv-parse';
+import fs from 'fs';
+
+import { BrandsRepository } from '../../repositories/implementations/BrandsRepository';
+
+interface IImportBrand {
+  name: string;
+  history: string;
+}
+
 class ImportBrandUseCase {
-  execute(file: Express.Multer.File): void {
-    console.log(file);
+  constructor(private brandsRepository: BrandsRepository) {}
+
+  loadBrands(file: Express.Multer.File): Promise<IImportBrand[]> {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path);
+      const brands: IImportBrand[] = [];
+
+      const parseFile = parse();
+
+      stream.pipe(parseFile);
+
+      parseFile
+        .on('data', async line => {
+          const [name, history] = line;
+
+          brands.push({
+            name,
+            history,
+          });
+        })
+        .on('end', () => {
+          resolve(brands);
+        })
+        .on('error', error => {
+          reject(error);
+        });
+    });
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const brands = await this.loadBrands(file);
+    console.log(brands);
   }
 }
 
--- src/modules/cars/useCases/importBrand/index.ts
+++ src/modules/cars/useCases/importBrand/index.ts
@@ -1,7 +1,9 @@
+import { BrandsRepository } from '../../repositories/implementations/BrandsRepository';
 import { ImportBrandController } from './ImportBrandController';
 import { ImportBrandUseCase } from './ImportBrandUseCase';
 
-const importBrandUseCase = new ImportBrandUseCase();
+const brandsRepository = BrandsRepository.getInstance();
+const importBrandUseCase = new ImportBrandUseCase(brandsRepository);
 const importBrandController = new ImportBrandController(importBrandUseCase);
 
 export { importBrandController };
