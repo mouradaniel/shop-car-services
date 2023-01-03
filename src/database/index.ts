c77f50f3c3f26c5caf25a1d295879c51b89ce6d1
Daniel Moura <daniel-moura@outlook.com>
2023-01-02 22:41:39 -0300

    fix: config typeorm

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,12 +1,3 @@
-import { DataSource } from 'typeorm';
+import { createConnection } from 'typeorm';
 
-const dataSource = new DataSource({
-  type: 'postgres',
-  host: 'database',
-  port: 5432,
-  username: 'test',
-  password: '1234',
-  database: 'shop-car',
-});
-
-dataSource.initialize();
+createConnection();
