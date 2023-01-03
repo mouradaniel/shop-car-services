427239622498b0fbaba8f40f88bab53abab76e8c
Daniel Moura <daniel-moura@outlook.com>
2023-01-02 22:42:31 -0300

    feat: update patch name to entities

--- src/modules/cars/entities/Brand.ts
+++ src/modules/cars/entities/Brand.ts
@@ -1,9 +1,18 @@
+import { Entity, PrimaryColumn, Column, CreateDateColumn } from 'typeorm';
 import { v4 as uuidV4 } from 'uuid';
 
+@Entity('brands')
 class Brand {
+  @PrimaryColumn()
   id?: string;
+
+  @Column()
   name: string;
+
+  @Column()
   history: string;
+
+  @CreateDateColumn()
   created_at: Date;
 
   constructor() {
