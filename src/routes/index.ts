d7e0f99f7d83a6048451f303747b0a65fc78b9f0
Daniel Moura <daniel-moura@outlook.com>
2023-01-04 01:29:25 -0300

    feat: create token for user

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 
+import { authenticateRoutes } from './authenticate.routes';
 import { brandsRoutes } from './brands.routes';
 import { specificationRoutes } from './specifications.routes';
 import { usersRoutes } from './users.routes';
@@ -9,5 +10,6 @@ const router = Router();
 router.use('/brands', brandsRoutes);
 router.use('/specifications', specificationRoutes);
 router.use('/users', usersRoutes);
+router.use(authenticateRoutes);
 
 export { router };
