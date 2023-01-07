e4b0032a8dd220e0fe0792565627615b5d04d4cc
Daniel Moura <daniel-moura@outlook.com>
2023-01-07 13:14:53 -0300

    feat: create a middleware to usage a custom error on app

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import sweaggerUi from 'swagger-ui-express';
 
 import { router } from './routes';
@@ -9,6 +10,7 @@ import swaggerFile from './swagger.json';
 import './database';
 
 import './shared/container';
+import { AppError } from './errors/AppError';
 
 const app = express();
 
@@ -17,6 +19,21 @@ app.use(express.json());
 app.use('/docs', sweaggerUi.serve, sweaggerUi.setup(swaggerFile));
 
 app.use(router);
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      message: error.message,
+    });
+  }
+
+  return res.status(500).json({
+    status: 'error',
+    messagE: `Internal server error - ${error.message}`,
+  });
+
+  next();
+});
 
 app.listen(3000, () => {
   console.log('Server is running on 3000');
