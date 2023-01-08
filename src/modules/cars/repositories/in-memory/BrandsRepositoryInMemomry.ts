9d4f262d73ca0d70d8c039cac202a40e5dd7dcab
Daniel Moura <daniel-moura@outlook.com>
2023-01-08 15:16:02 -0300

    feat: create brands repository in memomry

---
+++ src/modules/cars/repositories/in-memory/BrandsRepositoryInMemomry.ts
@@ -0,0 +1,29 @@
+import { Brand } from '../../entities/Brand';
+import { IBrandsRepository, ICreateBrandDTO } from '../IBrandsRepository';
+
+class BrandsRepositoryInMemory implements IBrandsRepository {
+  brands: Brand[] = [];
+
+  async findByName(name: string): Promise<Brand> {
+    const brand = this.brands.find(brand => brand.name === name);
+    return brand;
+  }
+
+  async list(): Promise<Brand[]> {
+    const { brands } = this;
+    return brands;
+  }
+
+  async create({ name, history }: ICreateBrandDTO): Promise<void> {
+    const brand = new Brand();
+
+    Object.assign(brand, {
+      name,
+      history,
+    });
+
+    this.brands.push(brand);
+  }
+}
+
+export { BrandsRepositoryInMemory };
