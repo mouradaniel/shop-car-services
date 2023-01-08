b76ab8c3ecd5860dba1da76cdd08063d82566d94
Daniel Moura <daniel-moura@outlook.com>
2023-01-08 16:12:30 -0300

    refactor: shared container imports

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,11 +1,11 @@
 import { container } from 'tsyringe';
 
-import { UsersRepository } from '../../modules/accounts/repositories/implementations/UsersRepository';
-import { IUsersRepository } from '../../modules/accounts/repositories/IUsersRepository';
-import { IBrandsRepository } from '../../modules/cars/repositories/IBrandsRepository';
-import { BrandsRepository } from '../../modules/cars/repositories/implementations/BrandsRepository';
-import { SpecificationsRepository } from '../../modules/cars/repositories/implementations/SpecificationsRepository';
-import { ISpecificationsRepository } from '../../modules/cars/repositories/ISpecificationsRepository';
+import { UsersRepository } from '@modules/accounts/repositories/implementations/UsersRepository';
+import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
+import { IBrandsRepository } from '@modules/cars/repositories/IBrandsRepository';
+import { BrandsRepository } from '@modules/cars/repositories/implementations/BrandsRepository';
+import { SpecificationsRepository } from '@modules/cars/repositories/implementations/SpecificationsRepository';
+import { ISpecificationsRepository } from '@modules/cars/repositories/ISpecificationsRepository';
 
 container.registerSingleton<IBrandsRepository>(
   'BrandsRepository',
