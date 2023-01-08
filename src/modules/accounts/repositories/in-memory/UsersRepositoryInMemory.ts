126bc520f44662306259d1cfc43c16a062ed5b32
Daniel Moura <daniel-moura@outlook.com>
2023-01-08 15:36:12 -0300

    feat: create accounts repository in memory

---
+++ src/modules/accounts/repositories/in-memory/UsersRepositoryInMemory.ts
@@ -0,0 +1,37 @@
+import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
+import { User } from '../../entities/User';
+import { IUsersRepository } from '../IUsersRepository';
+
+class UsersRepositoryInMemory implements IUsersRepository {
+  users: User[] = [];
+
+  async create({
+    name,
+    email,
+    password,
+    document,
+    username,
+  }: ICreateUserDTO): Promise<void> {
+    const user = new User();
+
+    Object.assign(user, {
+      name,
+      email,
+      password,
+      document,
+      username,
+    });
+
+    this.users.push(user);
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    return this.users.find(user => user.email === email);
+  }
+
+  async findById(id: string): Promise<User> {
+    return this.users.find(user => user.id === id);
+  }
+}
+
+export { UsersRepositoryInMemory };
