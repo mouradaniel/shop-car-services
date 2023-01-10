64691cd88c9b80ca988dd21c19fbc82c0270df0a
Daniel Moura <daniel-moura@outlook.com>
2023-01-10 16:32:31 -0300

    feat: init create car use case

---
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.ts
@@ -0,0 +1,34 @@
+import { inject, injectable } from 'tsyringe';
+
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+
+interface IRequest {
+  name: string;
+  description: string;
+  listPrice: number;
+  salePrice: number;
+}
+
+@injectable()
+class CreateCarUseCase {
+  constructor(
+    @inject('CarsRepository')
+    private carsRepository: ICarsRepository,
+  ) {}
+
+  async execute({
+    name,
+    description,
+    listPrice,
+    salePrice,
+  }: IRequest): Promise<void> {
+    this.carsRepository.create({
+      name,
+      description,
+      listPrice,
+      salePrice,
+    });
+  }
+}
+
+export { CreateCarUseCase };
