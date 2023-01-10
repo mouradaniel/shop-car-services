c3e8768c4fbcea006c512a0ac0fc053e0037a471
Daniel Moura <daniel-moura@outlook.com>
2023-01-10 16:32:49 -0300

    test: init create car use case

---
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -0,0 +1,22 @@
+import { CarsRepositoryInMemory } from '@modules/cars/repositories/in-memory/CarsRepositoryInMemory';
+
+import { CreateCarUseCase } from './CreateCarUseCase';
+
+let createCarUseCase: CreateCarUseCase;
+let carsRepositoryInMemory: CarsRepositoryInMemory;
+
+describe('Create Car', () => {
+  beforeEach(() => {
+    carsRepositoryInMemory = new CarsRepositoryInMemory();
+    createCarUseCase = new CreateCarUseCase(carsRepositoryInMemory);
+  });
+
+  it('Should be able to create a new car', async () => {
+    await createCarUseCase.execute({
+      name: 'Car Test',
+      description: 'Car Test Description',
+      listPrice: 30000,
+      salePrice: 27000,
+    });
+  });
+});
