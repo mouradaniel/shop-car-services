f8a5d0829848cbbcd113532970b23900516811c0
Daniel Moura <daniel-moura@outlook.com>
2023-01-11 13:14:50 -0300

    feat: implement cars routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import { authenticateRoutes } from './authenticate.routes';
 import { brandsRoutes } from './brands.routes';
+import { carsRoutes } from './cars.routes';
 import { specificationRoutes } from './specifications.routes';
 import { usersRoutes } from './users.routes';
 
@@ -10,6 +11,7 @@ const router = Router();
 router.use('/brands', brandsRoutes);
 router.use('/specifications', specificationRoutes);
 router.use('/users', usersRoutes);
+router.use('/cars', carsRoutes);
 router.use(authenticateRoutes);
 
 export { router };
