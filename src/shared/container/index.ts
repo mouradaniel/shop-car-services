13e65611aa2bad0d51281dec2fbafc9ceb603ffd
Daniel Moura <daniel-moura@outlook.com>
2023-01-11 13:13:47 -0300

    feat: register singleton cars repository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -3,8 +3,10 @@ import { container } from 'tsyringe';
 import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
 import { BrandsRepository } from '@modules/cars/infra/typeorm/repositories/BrandsRepository';
+import { CarsRepository } from '@modules/cars/infra/typeorm/repositories/CarsRepository';
 import { SpecificationsRepository } from '@modules/cars/infra/typeorm/repositories/SpecificationsRepository';
 import { IBrandsRepository } from '@modules/cars/repositories/IBrandsRepository';
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
 import { ISpecificationsRepository } from '@modules/cars/repositories/ISpecificationsRepository';
 
 container.registerSingleton<IBrandsRepository>(
@@ -21,3 +23,5 @@ container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
 );
+
+container.registerSingleton<ICarsRepository>('CarsRepository', CarsRepository);
