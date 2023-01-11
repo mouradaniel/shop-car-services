851ebf45dfbff1aa2f04dc979cb9df50aefbb5ed
Daniel Moura <daniel-moura@outlook.com>
2023-01-11 13:13:12 -0300

    feat: create car controller

---
+++ src/modules/cars/useCases/createCar/CreateCarController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { CreateCarUseCase } from './CreateCarUseCase';
+
+class CreateCarController {
+  async handle(req: Request, res: Response): Promise<Response> {
+    const { name, description, listPrice, salePrice } = req.body;
+
+    const createCarUseCase = container.resolve(CreateCarUseCase);
+
+    await createCarUseCase.execute({ name, description, listPrice, salePrice });
+
+    return res.status(201).send();
+  }
+}
+
+export { CreateCarController };
