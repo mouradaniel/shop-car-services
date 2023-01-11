4107de5af8bff9deea290f4acac8f7ba7e366b3c
Daniel Moura <daniel-moura@outlook.com>
2023-01-11 13:12:44 -0300

    feat: init cars repository

---
+++ src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -0,0 +1,32 @@
+import { getRepository, Repository } from 'typeorm';
+
+import { ICreateCarDTO } from '@modules/cars/dtos/ICreateCarDTO';
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+
+import { Car } from '../entities/Car';
+
+class CarsRepository implements ICarsRepository {
+  private repository: Repository<Car>;
+
+  constructor() {
+    this.repository = getRepository(Car);
+  }
+
+  async create({
+    name,
+    description,
+    listPrice,
+    salePrice,
+  }: ICreateCarDTO): Promise<void> {
+    const car = this.repository.create({
+      name,
+      description,
+      listPrice,
+      salePrice,
+    });
+
+    await this.repository.save(car);
+  }
+}
+
+export { CarsRepository };
