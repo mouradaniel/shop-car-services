9db625e50054121d9c3f4967771c1f37e6a519ab
Daniel Moura <daniel-moura@outlook.com>
2023-01-13 16:33:38 -0300

    feat: add find all on list cars interface repository

--- src/modules/cars/repositories/ICarsRepository.ts
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -3,6 +3,7 @@ import { Car } from '../infra/typeorm/entities/Car';
 
 interface ICarsRepository {
   create(data: ICreateCarDTO): Promise<Car>;
+  findAll(name?: string, brand_id?: string): Promise<Car[]>;
 }
 
 export { ICarsRepository };
