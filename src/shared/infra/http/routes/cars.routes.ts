4a3b614d421ef9b02943aa6b0cb2e4fb5d3f82c1
Daniel Moura <daniel-moura@outlook.com>
2023-01-13 16:36:26 -0300

    feat: implement list cars on car routes

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 
 import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController';
+import { ListCarsController } from '@modules/cars/useCases/listCars/ListCarsController';
 
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
+const listCarsController = new ListCarsController();
 
-carsRoutes.post('', createCarController.handle);
+carsRoutes.post('/', createCarController.handle);
+carsRoutes.get('/', listCarsController.handle);
 
 export { carsRoutes };
