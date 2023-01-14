7e28aa2b2e06d6975c4529fbfce4eb3a1fc9fae4
Daniel Moura <daniel-moura@outlook.com>
2023-01-14 15:31:18 -0300

    feat: add method find by id on interface car repository

--- src/modules/cars/repositories/ICarsRepository.ts
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -4,6 +4,7 @@ import { Car } from '../infra/typeorm/entities/Car';
 interface ICarsRepository {
   create(data: ICreateCarDTO): Promise<Car>;
   findAll(name?: string, brand_id?: string): Promise<Car[]>;
+  findById(id: string): Promise<Car>;
 }
 
 export { ICarsRepository };
