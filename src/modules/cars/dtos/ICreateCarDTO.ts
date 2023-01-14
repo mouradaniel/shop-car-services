d8e27812b6c33ecee2ba2d4eacc3d9fd7b6195ab
Daniel Moura <daniel-moura@outlook.com>
2023-01-14 15:50:33 -0300

    feat: add specifications to create car dto

--- src/modules/cars/dtos/ICreateCarDTO.ts
+++ src/modules/cars/dtos/ICreateCarDTO.ts
@@ -1,9 +1,12 @@
+import { Specification } from '../infra/typeorm/entities/Specification';
+
 interface ICreateCarDTO {
   name: string;
   description: string;
   listPrice: number;
   salePrice: number;
-  brand_id: string;
+  brand_id?: string;
+  specifications?: Specification[];
 }
 
 export { ICreateCarDTO };
