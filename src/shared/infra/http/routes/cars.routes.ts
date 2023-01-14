1b883609ec4784740b5ace8fb346c51f09a2f88f
Daniel Moura <daniel-moura@outlook.com>
2023-01-14 16:18:10 -0300

    feat: implement endpoint to add specifications to the car

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,14 +1,25 @@
 import { Router } from 'express';
 
 import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController';
+import { CreateCarSpecificationController } from '@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController';
 import { ListCarsController } from '@modules/cars/useCases/listCars/ListCarsController';
+
+import { ensureAdmin } from '../middlewares/ensureAdmin';
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
 const listCarsController = new ListCarsController();
+const createCarSpecificationController = new CreateCarSpecificationController();
 
 carsRoutes.post('/', createCarController.handle);
 carsRoutes.get('/', listCarsController.handle);
+carsRoutes.post(
+  '/specifications/:id',
+  ensureAuthenticated,
+  ensureAdmin,
+  createCarSpecificationController.handle,
+);
 
 export { carsRoutes };
