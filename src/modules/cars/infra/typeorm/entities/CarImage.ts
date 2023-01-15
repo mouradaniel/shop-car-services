2d2992716d9566f45a26bfba8d072675d037a5bf
Daniel Moura <daniel-moura@outlook.com>
2023-01-15 14:15:41 -0300

    fix: add id on car image entity

--- src/modules/cars/infra/typeorm/entities/CarImage.ts
+++ src/modules/cars/infra/typeorm/entities/CarImage.ts
@@ -1,4 +1,5 @@
 import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
+import { v4 as uuidV4 } from 'uuid';
 
 @Entity('cars_image')
 class CarImage {
@@ -13,6 +14,12 @@ class CarImage {
 
   @CreateDateColumn()
   created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
 }
 
 export { CarImage };
