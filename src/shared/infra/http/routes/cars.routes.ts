c2fc0d6f17fed1f1c93a117f0bf773ed28eee975
Daniel Moura <daniel-moura@outlook.com>
2023-01-15 14:49:07 -0300

    feat: implement endpoint to upload cars images

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,25 +1,41 @@
 import { Router } from 'express';
+import multer from 'multer';
 
+import uploadConfig from '@config/upload';
 import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController';
 import { CreateCarSpecificationController } from '@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController';
 import { ListCarsController } from '@modules/cars/useCases/listCars/ListCarsController';
+import { UploadCarImagesController } from '@modules/cars/useCases/uploadCarImage/UploadCarImagesController';
 
 import { ensureAdmin } from '../middlewares/ensureAdmin';
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
 const carsRoutes = Router();
 
+const upload = multer(uploadConfig.upload('./tmp/cars'));
+
 const createCarController = new CreateCarController();
 const listCarsController = new ListCarsController();
 const createCarSpecificationController = new CreateCarSpecificationController();
+const uploadCarImagesControlles = new UploadCarImagesController();
 
 carsRoutes.post('/', createCarController.handle);
+
 carsRoutes.get('/', listCarsController.handle);
+
 carsRoutes.post(
   '/specifications/:id',
   ensureAuthenticated,
   ensureAdmin,
   createCarSpecificationController.handle,
+);
+
+carsRoutes.post(
+  '/images/:id',
+  ensureAuthenticated,
+  ensureAdmin,
+  upload.array('images'),
+  uploadCarImagesControlles.handle,
 );
 
 export { carsRoutes };
