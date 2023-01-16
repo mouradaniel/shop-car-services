155ef3f9d1f17982a2ceec5298222eca16326630
Daniel Moura <daniel-moura@outlook.com>
2023-01-16 01:42:35 -0300

    refactor: rename and export app

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,39 +1,4 @@
-import 'reflect-metadata';
-
-import express, { NextFunction, Request, Response } from 'express';
-import 'express-async-errors';
-import sweaggerUi from 'swagger-ui-express';
-
-import { AppError } from '@shared/errors/AppError';
-import createConnection from '@shared/infra/typeorm';
-
-import '@shared/container';
-import swaggerFile from '../../../swagger.json';
-import { router } from './routes';
-
-createConnection();
-const app = express();
-
-app.use(express.json());
-
-app.use('/docs', sweaggerUi.serve, sweaggerUi.setup(swaggerFile));
-
-app.use(router);
-
-app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
-  if (error instanceof AppError) {
-    return res.status(error.statusCode).json({
-      message: error.message,
-    });
-  }
-
-  return res.status(500).json({
-    status: 'error',
-    messagE: `Internal server error - ${error.message}`,
-  });
-
-  next();
-});
+import { app } from './app';
 
 app.listen(3000, () => {
   console.log('Server is running on 3000');
