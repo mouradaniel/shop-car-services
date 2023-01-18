b377e0175201d2acbf84f78f947ca4e943fa0373
Daniel Moura <daniel-moura@outlook.com>
2023-01-18 18:04:50 -0300

    feat: implement endpoint to get the profile

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -1,11 +1,17 @@
 import { Router } from 'express';
 
 import { CreateUserController } from '@modules/accounts/useCases/createUser/CreateUserController';
+import { ProfileUserController } from '@modules/accounts/useCases/profileUser/ProfileUserController';
+
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
 const usersRoutes = Router();
 
 const createUserController = new CreateUserController();
+const profileUserController = new ProfileUserController();
 
 usersRoutes.post('', createUserController.handle);
+
+usersRoutes.get('/profile', ensureAuthenticated, profileUserController.handle);
 
 export { usersRoutes };
