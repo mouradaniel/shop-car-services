40033104a52a3d70d6debe9bd4ab74bc7636ca15
Daniel Moura <daniel-moura@outlook.com>
2023-01-20 02:56:47 -0300

    test: create new user with same email

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.spec.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.spec.ts
@@ -1,5 +1,6 @@
 import { ICreateUserDTO } from '@modules/accounts/dtos/ICreateUserDTO';
 import { UsersRepositoryInMemory } from '@modules/accounts/repositories/in-memory/UsersRepositoryInMemory';
+import { AppError } from '@shared/errors/AppError';
 
 import { CreateUserUseCase } from './CreateUserUseCase';
 
@@ -26,5 +27,20 @@ describe('Create User', () => {
     const createdUser = await usersRepositoryInMemory.findByEmail(user.email);
 
     expect(createdUser).toHaveProperty('id');
+  });
+
+  it('Should not be able to create a new user with same email', async () => {
+    expect(async () => {
+      const user: ICreateUserDTO = {
+        name: 'User Test',
+        email: 'user@test.com',
+        password: '1234',
+        document: '11122233345',
+        username: 'user_test',
+      };
+
+      await createUserUseCase.execute(user);
+      await createUserUseCase.execute(user);
+    }).rejects.toBeInstanceOf(AppError);
   });
 });
